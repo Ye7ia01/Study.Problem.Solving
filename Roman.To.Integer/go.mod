a8de279df7c05ee0b0d821d8109b819c5464a877
Ye7ia01 <ye7iaabdelhady@gmail.com>
2026-09-20 14:52:28 +0300

    Add Roman.To.Integer

---
+++ Roman.To.Integer/go.mod
@@ -0,0 +1,3 @@
+module github.com/Ye7ia01/Problem.Solving/Roman.To.Integer
+
+go 1.25.0
---
+++ Roman.To.Integer/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func romanToInt(s string) int {
+
+	roman := map[string]int{"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+	ans := 0
+	for i := 0; i < len(s); i++ {
+		// Check first unique cases
+		if len(s) > i+1 {
+			if roman[string(s[i])] < roman[string(s[i+1])] {
+				ans += roman[string(s[i+1])] - roman[string(s[i])]
+				i++
+				continue
+			}
+		}
+		ans += roman[string(s[i])]
+		continue
+	}
+	return ans
+}
+
+func main() {
+
+	fmt.Println(romanToInt("MCMXCIV"))
+}
